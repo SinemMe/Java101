35138911f017426dc5e25a20a2fe344d2a3d6872
SinemMe <didem.p1997@gmail.com>
2024-09-30 10:25:37 +0300

    YildizlarIleP
    Odev-YildizlarIleP

---
+++ YildizlarIleP/src/Main.java
@@ -0,0 +1,25 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        String[][] letter = new String[6][4];
+
+        for (int i = 0; i < letter.length; i++) {
+            for (int j = 0; j < letter[i].length; j++) {
+                if (i == 0 || i == 1 || i == 2 ) {
+                    letter[i][j] = " * ";
+                } else if (j == 0 ) {
+                    letter[i][j] = " * ";
+                } else {
+                    letter[i][j] = "  ";
+                }
+            }
+        }
+        for (String[] row : letter) {
+            for (String col : row) {
+                System.out.print(col);
+            }
+            System.out.println();
+        }
+    }
+}
